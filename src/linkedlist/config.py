0f0a9634438b8465eb1d55773a98313e4f682278
Raffael Thome <thome.raffael@gmail.com>
2013-11-18 18:02:28 +0100

    Add navbar

--- src/linkedlist/config.py
+++ src/linkedlist/config.py
@@ -1,3 +1,7 @@
-DATABASE = "linkedlist.db"
+import os, os.path
+
+base_path = os.getcwd()
+
+DATABASE = os.path.join(base_path, "linkedlist.db")
 DEBUG = True
 SECRET_KEY = "7B+PL102DpIf5bULXiER1t/dqsf1uLmqSmdjNR8L1+Q2MFnlNL93Ij09p2fxB6gw5mMh2Mmu4UG3G5UR6b9org"
--- src/linkedlist/linkedlist.py
+++ src/linkedlist/linkedlist.py
@@ -6,15 +6,15 @@ import models, config
 app = Flask(__name__)
 app.config.from_object(config)
 
-def base_js():
-	return ["https://code.jquery.com/jquery.js", url_for("static", filename="js/bootstrap.min.js")]
-
-def base_css():
-	return [url_for("static", filename="css/bootstrap.min.css")]
+@app.before_request
+def init_base_args():
+	g.base_template_args = {}
+	g.base_template_args.update({"js": ["https://code.jquery.com/jquery.js", url_for("static", filename="js/bootstrap.min.js")], \
+								 "css": [url_for("static", filename="css/bootstrap.min.css")]})
 
 @app.route("/")
 def index():
-	return render_template("index.html", base_js=base_js(), base_css=base_css())
+	return render_template("index.html", base=g.base_template_args)
 
 def run():
 	if not os.path.isfile(config.DATABASE):

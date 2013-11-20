3bfb977fbcd819dc5bc0f53658eeafbf09fe863d
Raffael Thome <thome.raffael@gmail.com>
2013-11-20 11:37:46 +0100

    Work on login template

--- src/linkedlist/linkedlist.py
+++ src/linkedlist/linkedlist.py
@@ -12,13 +12,24 @@ def init_base_args():
 	g.base_template_args.update({"js": ["https://code.jquery.com/jquery.js", url_for("static", filename="js/bootstrap.min.js")], \
 								 "css": [url_for("static", filename="css/bootstrap.min.css")]})
 
+@app.route("/register")
+def register():
+	return render_template("register.html", base=g.base_template_args)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # authenticate and log in here
+        return redirect(url_for("index"))
+    else:
+        return render_template("login.html", base=g.base_template_args)
+
 @app.route("/")
 def index():
-	return render_template("index.html", base=g.base_template_args)
-
-@app.route("/login")
-def login():
-	return render_template("login.html", base=g.base_template_args)
+    if session["logged_in"]:
+        return render_template("index.html", base=g.base_template_args)
+    else:
+        return redirect(url_for("login"))
 
 def run():
 	if not os.path.isfile(config.DATABASE):

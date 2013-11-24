198ff2ea11638f4692b05e113bdc43541db484d3
Raffael Thome <thome.raffael@gmail.com>
2013-11-24 17:08:52 +0100

    Add routing rules for admin pages

--- src/linkedlist/linkedlist.py
+++ src/linkedlist/linkedlist.py
@@ -23,11 +23,16 @@ def inject_base_args():
     d["base"] = dict(js=["https://code.jquery.com/jquery.js", url_for("static", filename="js/bootstrap.min.js")], css=[url_for("static", filename="css/bootstrap.min.css")])
     return d
 
+# user pages
 app.add_url_rule("/", view_func=views.IndexView.as_view("index"))
 app.add_url_rule("/register", view_func=views.RegistrationView.as_view("register"))
 app.add_url_rule("/login", view_func=views.LoginView.as_view("login"))
 app.add_url_rule("/logout", view_func=views.LogoutView.as_view("logout"))
 app.add_url_rule("/add", view_func=views.AddView.as_view("add"))
+
+# admin pages
+app.add_url_rule("/admin_login", view_func=views.AdminLoginView.as_view("admin_login"))
+app.add_url_rule("/admin", view_func=views.AdminPanelView.as_view("admin_panel"))
 
 def run():
 	if not os.path.isfile(config.DATABASE):

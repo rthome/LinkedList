f8e0741c05f21a4a5940d9bf334c499a640f2a76
Raffael Thome <thome.raffael@gmail.com>
2013-11-24 17:07:15 +0100

    Add a hashed admin password to config

--- src/linkedlist/config.py
+++ src/linkedlist/config.py
@@ -5,3 +5,5 @@ base_path = os.getcwd()
 DATABASE = os.path.join(base_path, "linkedlist.db")
 DEBUG = True
 SECRET_KEY = "7B+PL102DpIf5bULXiER1t/dqsf1uLmqSmdjNR8L1+Q2MFnlNL93Ij09p2fxB6gw5mMh2Mmu4UG3G5UR6b9org"
+
+admin_pw_hash = "7d55f46bf71846c9ac2f9d53d9199caa$2b9fd788dbd503b40804a0e5a5399de022f608928f169e9517de29e7634cd86bbc94e42f07557eda0d68a3291fd476641c931f9ce295e5c3526843eaa73c181e"

d713507148641bea56fccd2b07efce67577665c4
Raffael Thome <thome.raffael@gmail.com>
2014-05-13 21:00:46 +0200

    improve envvar config handling
    Also only allow database uri and secret key to be set for now

--- linkedlist/factory.py
+++ linkedlist/factory.py
@@ -7,32 +7,31 @@ from .core import db, security
 from .helpers import register_blueprints
 from .models import User, Role
 
-# Constant, previx for environment variables for linkedlist config options
-LINKEDLIST_ENVVAR_PREFIX = "LINKEDLIST_CONFIG_"
-
 def find_environ_config_vars():
     """
-    Find environment variables matching LINKEDLIST_CONFIG_<name>
-    and returns a dict of them
+    Find environment variables containing config data and return a dict of them
     """
+    # only allow secret key and database uri for now
+    envvars = ["SQLALCHEMY_DATABASE_URI", "SECRET_KEY"]
     results = {}
     for key, value in os.environ.iteritems():
-        if key.startswith(LINKEDLIST_ENVVAR_PREFIX):
-            trimmed_key = key[len(LINKEDLIST_ENVVAR_PREFIX):]
-            results[trimmed_key] = value
+        if key in envvars:
+            results[key] = value
     return results
 
 def create_app(name, path, settings_override=None,
                register_security_blueprint=True):
     """Returns a Flask application object"""
 
-    environ_config_override = find_environ_config_vars()
-
     app = Flask(name, instance_relative_config=True)
     app.config.from_object("linkedlist.config") # public config
     app.config.from_pyfile("config.py", silent=True) # instance config
-    app.config.from_object(environ_config_override) # environment variable override
     app.config.from_object(settings_override) # argument override
+
+    # patch in envvar config
+    environ_config_override = find_environ_config_vars()
+    for key, value in environ_config_override.iteritems():
+        app.config[key] = value
 
     db.init_app(app)
     security.init_app(app, SQLAlchemyUserDatastore(db, User, Role),
@@ -42,5 +41,6 @@ def create_app(name, path, settings_override=None,
     # create database tables
     with app.app_context():
         db.create_all()
+        print app.config
 
     return app

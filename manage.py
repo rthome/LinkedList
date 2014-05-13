e02dff030479ef394a5b4a2f17fc21d2aee845a9
Raffael Thome <thome.raffael@gmail.com>
2014-05-13 21:22:49 +0200

    Implement pep8 checks in manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,20 @@
-from flask_script import Manager
+from flask_script import Manager, Command
+import pep8
 
 from linkedlist.api import create_app
 from linkedlist.manage import CreateUserCommand, DeleteUserCommand, ListUsersCommand
+
+class Pep8Command(Command):
+
+    def run(self):
+        style_guide = pep8.StyleGuide()
+        style_guide.input_dir(".")
 
 manager = Manager(create_app())
 manager.add_command("create_user", CreateUserCommand())
 manager.add_command("delete_user", DeleteUserCommand())
 manager.add_command("list_users", ListUsersCommand())
+manager.add_command("pep8", Pep8Command())
 
 if __name__ == "__main__":
     manager.run()

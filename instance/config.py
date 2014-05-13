4d4eb42e217a6aae0aef20fe07fbe299ebe71c46
Raffael Thome <thome.raffael@gmail.com>
2014-05-13 13:48:59 +0200

    Get rid of intermediate LinkedList folder

---
+++ instance/config.py
@@ -0,0 +1,6 @@
+# Private config file
+
+DEBUG = True
+SECRET_KEY = "089yroqnflknsdfp9yfo2g3br2lhsofihnfdnsfuwgofihsnpdfns"
+
+SQLALCHEMY_DATABASE_URI = "postgresql://linkedlist:7412@localhost/linkedlist"

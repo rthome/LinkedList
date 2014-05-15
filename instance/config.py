f5ee8fb41a6e13250eca9cf73bf8ff7a2fd38a4d
Raffael Thome <thome.raffael@gmail.com>
2014-05-15 09:42:17 +0200

    Remove instance config file

--- instance/config.py
+++
@@ -1,6 +0,0 @@
-# Private config file
-
-DEBUG = True
-SECRET_KEY = "089yroqnflknsdfp9yfo2g3br2lhsofihnfdnsfuwgofihsnpdfns"
-
-SQLALCHEMY_DATABASE_URI = "postgresql://linkedlist:7412@localhost/linkedlist"

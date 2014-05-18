c0b34e9db2c951e7c865c1a9a60de5f337b9a7c4
Raffael Thome <thome.raffael@gmail.com>
2014-05-18 16:19:48 +0200

    On debug runs, use host and port from os.environ

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,12 @@ application = DispatcherMiddleware(frontend.create_app(), {
 })
 
 if __name__ == "__main__":
-    run_simple("0.0.0.0", 5000, application,
+    import os
+    host = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        port = int(os.environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        port = 5000
+    run_simple(host, port, application,
                use_reloader=True,
                use_debugger=True)

979596d6ce7e7b586fe42839f8a82ed38cf6c561
Raffael Thome <thome.raffael@gmail.com>
2014-05-21 08:41:35 +0200

    Split off custom css into separate assets bundle

--- linkedlist/frontend/assets.py
+++ linkedlist/frontend/assets.py
@@ -1,19 +1,21 @@
 from flask_assets import Environment, Bundle
 
-css_all = Bundle("css/bootstrap.css",
-                 "css/bootstrap-theme.css",
-                 "css/linkedlist.css",
-                 filters="cssmin", output="css/linkedlist_all.min.css")
+css_vendor = Bundle("css/bootstrap.css",
+                    "css/bootstrap-theme.css",
+                    filters="cssmin", output="css/linkedlist_vendor.min.css")
 
-js_all = Bundle("js/jquery-1.11.1.min.js",
-                "js/bootstrap.js",
-                filters="jsmin", output="js/linkedlist_all.min.js")
+js_vendor = Bundle("js/jquery-1.11.1.min.js",
+                   "js/bootstrap.js",
+                   filters="jsmin", output="js/linkedlist_vendor.min.js")
+
+css_linkedlist = Bundle("css/linkedlist.css")
 
 
 def init_app(app):
     assets = Environment(app)
-    assets.register("css_all", css_all)
-    assets.register("js_all", js_all)
+    assets.register("css_vendor", css_vendor)
+    assets.register("js_vendor", js_vendor)
+    assets.register("css_linkedlist", css_linkedlist)
     assets.manifest = 'cache' if not app.debug else False
     assets.cache = not app.debug
     assets.debug = app.debug
